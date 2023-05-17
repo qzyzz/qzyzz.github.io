4ef8be0a0740fb0f5a0cb4d37e02b952a6ed3c35
qizhiyang <zhiyang.qi@hahacloud.com>
2023-05-17 21:39:37 +0800

    fix: bugFix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,12 +7,11 @@ module.exports = {
   title: "知非",
   tagline: "记录前端学习",
   titleDelimiter: "-",
-  url: "https://qzyzz.github.io/Blog",
-  baseUrl: "/",
+  url: "https://qzyzz.github.io",
+  baseUrl: "/BLOG/",
   favicon: "img/logo.png",
   organizationName: "qzyzz", // Usually your GitHub org/user name.
   projectName: "BLOG", // Usually your repo name.
-  deploymentBranch:'gh-pages',
   themeConfig: {
     image: "img/logo.png",
     announcementBar: {
